0ba9b09f7708f19501c1ca1cd6b1c979888c6745
Tom Hickerson <tom@thehillsidegroup.com>
2023-04-20 12:10:04 +0300

    update some colorization of the messages

--- src/main/java/net/sf/nightworks/enums/PlayerMessage.java
+++ src/main/java/net/sf/nightworks/enums/PlayerMessage.java
@@ -2,11 +2,11 @@ package net.sf.nightworks.enums;
 
 public enum PlayerMessage {
 	
-	YOU_HAVE_BEEN_KILLED(1, "You have been KILLED!\n"),
-	WEAPON_DOING_GREAT(2, "Your weapon whispers, 'You're doing great!'\n"),
-	WEAPON_GOOD_WORK(3, "Your weapon whispers, 'Keep up the good work!'\n"),
-	WEAPON_CAN_DO_IT(4, "Your weapon whispers, 'You can do it!'\n"),
-	WEAPON_RUN_AWAY(5, "Your weapon whispers, 'Run away! Run away!'\n"),
+	YOU_HAVE_BEEN_KILLED(1, "You have been {RKILLED{x!\n"),
+	WEAPON_DOING_GREAT(2, "Your weapon whispers, '{cYou're doing great!{x'\n"),
+	WEAPON_GOOD_WORK(3, "Your weapon whispers, '{cKeep up the good work!{x'\n"),
+	WEAPON_CAN_DO_IT(4, "Your weapon whispers, '{cYou can do it!{x'\n"),
+	WEAPON_RUN_AWAY(5, "Your weapon whispers, '{rRun away! Run away!{x'\n"),
 	WEAPON_GLOWS_BLUE(6, "Your weapon glows {cblue.{x"),
 	GODS_ARE_FURI(7, "The gods are infuriated!"),
 	CONSUMNED_BY_RAGE(8, "Your pulse races as you are consumned by rage!\n"),
