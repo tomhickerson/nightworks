e491a7efb30080a039c6c065a79fe43f96746967
Tom Hickerson <tom@thehillsidegroup.com>
2023-06-28 10:01:02 +0300

    updates to quests, add more types of quests

--- src/main/java/net/sf/nightworks/quests/SimpleCollectQuest.java
+++ src/main/java/net/sf/nightworks/quests/SimpleCollectQuest.java
@@ -2,11 +2,24 @@ package net.sf.nightworks.quests;
 
 import net.sf.nightworks.Nightworks;
 
+/**
+ * SimpleCollectQuest.java
+ * Examples: Collect five copper cogs, scattered around a given area
+ * Collect eight pieces of a puzzle in a given area, put them all in a paper bag
+ * Someday: -- collect different items and put them in one container?
+ * -- collect different items and put them in different containers?
+ */
 public class SimpleCollectQuest extends SimpleQuest {
 
     // private int[] vnumsToGet = new int[];
 
     private int questPoints = 0;
+    private int vnumToCollect = -1;
+    private int numberToCollect = -1;
+    private String areaToCollect = "midgaard.are";
+    private int vnumContainer = -1;
+    // is picking up the container a catalyst for starting the quest?
+    // container doesn't necessarily have to be involved
 
     public SimpleCollectQuest(String name) {
         super(name);
@@ -24,5 +37,45 @@ public class SimpleCollectQuest extends SimpleQuest {
     @Override
     public boolean isStandalone() {
         return false;
+    }
+
+    public int getQuestPoints() {
+        return questPoints;
+    }
+
+    public void setQuestPoints(int questPoints) {
+        this.questPoints = questPoints;
+    }
+
+    public int getVnumToCollect() {
+        return vnumToCollect;
+    }
+
+    public void setVnumToCollect(int vnumToCollect) {
+        this.vnumToCollect = vnumToCollect;
+    }
+
+    public int getNumberToCollect() {
+        return numberToCollect;
+    }
+
+    public void setNumberToCollect(int numberToCollect) {
+        this.numberToCollect = numberToCollect;
+    }
+
+    public String getAreaToCollect() {
+        return areaToCollect;
+    }
+
+    public void setAreaToCollect(String areaToCollect) {
+        this.areaToCollect = areaToCollect;
+    }
+
+    public int getVnumContainer() {
+        return vnumContainer;
+    }
+
+    public void setVnumContainer(int vnumContainer) {
+        this.vnumContainer = vnumContainer;
     }
 }
---
+++ src/main/java/net/sf/nightworks/quests/SimpleHuntQuest.java
@@ -0,0 +1,57 @@
+package net.sf.nightworks.quests;
+
+import java.util.ArrayList;
+
+/**
+ * SimpleHuntQuest
+ * The quest for hunting down several of one vnum of mobs and killing them
+ * Can also be for hunting down several vnums?  Nope, that is for the Defend Quest
+ */
+public class SimpleHuntQuest extends SimpleQuest {
+
+    private int vnumToKill = -1;
+    private int numberToKill = -1;
+    private ArrayList<Integer> rooms = new ArrayList<>();
+
+    public SimpleHuntQuest(String name) {
+        super(name);
+    }
+
+    public SimpleHuntQuest(int id, String name) {
+        super(id, name);
+    }
+
+    public int getVnumToKill() {
+        return vnumToKill;
+    }
+
+    public void setVnumToKill(int vnumToKill) {
+        this.vnumToKill = vnumToKill;
+    }
+
+    public int getNumberToKill() {
+        return numberToKill;
+    }
+
+    public void setNumberToKill(int numberToKill) {
+        this.numberToKill = numberToKill;
+    }
+
+    public ArrayList<Integer> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Integer> rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public boolean canRunAgain() {
+        return false;
+    }
+
+    @Override
+    public boolean isStandalone() {
+        return false;
+    }
+}
