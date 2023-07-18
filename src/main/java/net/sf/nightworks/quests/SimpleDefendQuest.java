159da627b7c0bec5562eacc9c842e703b769ac3b
Tom Hickerson <tom@thehillsidegroup.com>
2023-07-18 09:11:29 +0300

    minor fooling around with quests

--- src/main/java/net/sf/nightworks/quests/SimpleDefendQuest.java
+++ src/main/java/net/sf/nightworks/quests/SimpleDefendQuest.java
@@ -6,12 +6,26 @@ import java.util.ArrayList;
  * SimpleDefendQuest, where you defend a given area against a number of attackers
  * Attackers can be different mobs or not
  * Attackers will have the 'flee' flag activated
+ * Attackers will have to be placed by the quest
  * maybe need a setup quest interface, to run it?
  */
 public class SimpleDefendQuest extends SimpleQuest {
 
     private ArrayList<Integer> vnumMobs = new ArrayList<>();
     private ArrayList<Integer> rooms = new ArrayList<>();
+    private setup questSetup;
+
+    public setup getQuestSetup() {
+        return questSetup;
+    }
+
+    public void setQuestSetup(setup questSetup) {
+        this.questSetup = questSetup;
+    }
+
+    public void runSetup() {
+        this.questSetup.run();
+    }
 
     public ArrayList<Integer> getVnumMobs() {
         return vnumMobs;
@@ -40,5 +54,9 @@ public class SimpleDefendQuest extends SimpleQuest {
     @Override
     public boolean isStandalone() {
         return false;
+    }
+
+    public interface setup {
+        void run();
     }
 }
