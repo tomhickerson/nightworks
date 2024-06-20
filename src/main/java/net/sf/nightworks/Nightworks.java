1ac50be12632a4f34a02e4d42789f0b46b1aae49
Tom Hickerson <tom@thehillsidegroup.com>
2024-06-20 16:35:42 +0300

    updates and what nots

--- src/main/java/net/sf/nightworks/Nightworks.java
+++ src/main/java/net/sf/nightworks/Nightworks.java
@@ -2573,9 +2573,11 @@ public final class Nightworks {
     }
 
     public static boolean HAS_LORE(CHAR_DATA ch, PlayerLore pl, int level) {
-        int checkLevel = 0;
-        checkLevel = ch.pcdata.playerLoreLevel.get(pl);
-        return checkLevel <= level;
+        int checkLevel = -1;
+        if (ch.pcdata.playerLores.contains(pl)) {
+            checkLevel = ch.pcdata.playerLoreLevel.get(pl);
+        }
+        return checkLevel >= level;
     }
 
     public static boolean IS_DWARF(CHAR_DATA ch) {
